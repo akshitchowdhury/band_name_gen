cc4ff74d14ed34a1923020b196d25d33e4cd0efa
Pratikshit Chowdhury <101483800+akshitchowdhury@users.noreply.github.com>
2023-07-31 14:26:27 +0530

    Add files via upload

--- Band_name_gen/index.js
+++ Band_name_gen/index.js
@@ -2,16 +2,34 @@ import express from 'express'
 import {dirname} from 'path'
 import {fileURLToPath} from 'url'
 import bodyParser from 'body-parser'
-import { dir } from 'console'
-
 const app = express()
 
-const port = 3000
+const port = 3001
 const _dirname = dirname(fileURLToPath(import.meta.url))
-
+var bandname
 app.get("/", (req, res)=>{
 
     res.sendFile(_dirname+"/index.html")
 })
 
+app.use(bodyParser.urlencoded({extended:true}))
+
+
+
+function test (req, res, next){
+
+    console.log(req.body)
+    bandname = req.body["email"] + req.body["password"]
+    next()
+}
+
+
+app.use(test)
+
+app.post("/submit", (req,res)=>{
+    res.send(`<h1>Your band name generator is </h1> <h2> ${bandname} </h2>`)
+})
+app.listen(port,()=>{
+    console.log(`listenin on port ${port}`)
+})
 
