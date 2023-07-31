6067c1d5e9409e0fa64dc02c3e09c64b9b5bb835
Pratikshit Chowdhury <101483800+akshitchowdhury@users.noreply.github.com>
2023-07-31 13:43:27 +0530

    Add files via upload

--- Band_name_gen/index.js
+++ Band_name_gen/index.js
@@ -1,1 +1,17 @@
 import express from 'express'
+import {dirname} from 'path'
+import {fileURLToPath} from 'url'
+import bodyParser from 'body-parser'
+import { dir } from 'console'
+
+const app = express()
+
+const port = 3000
+const _dirname = dirname(fileURLToPath(import.meta.url))
+
+app.get("/", (req, res)=>{
+
+    res.sendFile(_dirname+"/index.html")
+})
+
+
